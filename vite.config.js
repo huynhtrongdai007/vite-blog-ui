134c89600c713ba2ecc86e9547da3b9021ad281e
huynhtrongdai007 <huynhtrongdaiz@gmail.com>
2026-05-30 22:38:25 +0700

    upcode

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import {viteEjs} from "./ejs-vite-plugin";
+
+const root = fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig({
+    plugins:[viteEjs()],
+    build: {
+        rollupOptions: {
+            input: {
+                index: `${root}index.html`,
+                about: `${root}about.html`,
+                contact: `${root}contact.html`,
+                register: `${root}register.html`,
+                login: `${root}login.html`,
+                category: `${root}category.html`,
+                "post-detail": `${root}post-detail.html`,
+                "my-posts": `${root}my-posts.html`,
+                write: `${root}write.html`,
+                elements: `${root}elements.html`,
+            },
+        },
+    },
+});
